6d62c82826f276be14330f1729b3e9b9cf2087d2
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-05-19 11:41:49 -0500

    db setup & connection complete

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
